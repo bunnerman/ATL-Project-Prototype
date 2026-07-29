032387146cebf07b32dbb7fa47c7ea04ae4bb106
bunnerman <parnilrbalawade@gmail.com>
2026-07-29 18:04:59 +0530

    Update streamlit-testing.py

--- streamlit-testing.py
+++ streamlit-testing.py
@@ -2,9 +2,10 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt;
 
+# example of smth like app builds ig?
 df0 = {
     "Version": [1.0, 1.3, 2.0, 2.5, 2.7, 2.8, 3.0, 3.1, 3.2],
-    "Performance": ["0.52", "0.56", "0.67", "0.84", "0.86", "0.88", "0.95", "0.96", "0.97"]
+    "Performance": [0.52, 0.56, 0.67, 0.84, 0.86, 0.88, 0.95, 0.96, 0.97]
 }
 df = pd.DataFrame(df0)
 
@@ -12,6 +13,8 @@ df.plot(x = 'Version', y = 'Performance', kind = 'scatter')
 
 st.title("Basic ATL Project Prototype")
 
+
+st.text("This graph is simply for testing if other Python Libraries work on Streamlit Community Cloud")
 fig, ax = plt.subplots()
 df.plot(x = 'Version', y = 'Performance', kind = 'scatter', ax = ax)
 
