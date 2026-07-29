2132218616096aab9832c8c76f901c81417af87e
bunnerman <parnilrbalawade@gmail.com>
2026-07-29 18:01:04 +0530

    Update streamlit-testing.py

--- streamlit-testing.py
+++ streamlit-testing.py
@@ -1,9 +1,18 @@
-# Only used for testing purposes
-
 import pandas as pd
-import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt;
 
-st.title("ATL Prototype Testing")
+df0 = {
+    "Version": [1.0, 1.3, 2.0, 2.5, 2.7, 2.8, 3.0, 3.1, 3.2],
+    "Performance": ["0.52", "0.56", "0.67", "0.84", "0.86", "0.88", "0.95", "0.96", "0.97"]
+}
+df = pd.DataFrame(df0)
 
-st.badge("Hello", color="blue")
+df.plot(x = 'Version', y = 'Performance', kind = 'scatter')
+
+st.title("Basic ATL Project Prototype")
+
+fig, ax = plt.subplots()
+df.plot(x = 'Version', y = 'Performance', kind = 'scatter', ax = ax)
+
+st.pyplot(fig)
